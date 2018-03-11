ba7771070fa3d10df25bab94003d65bf0bed4fcb
vkostylev <kostylev.v@gmail.com>
2018-03-11 09:48:10 +0300

    Basic functionality

---
+++ src/com/kostylevv/Main.java
@@ -0,0 +1,18 @@
+package com.kostylevv;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today: " + today.toString());
+        System.out.println("EoW: " + (7 - today.getDayOfWeek().getValue()));
+        System.out.println("EoM: " + (today.lengthOfMonth() - today.getDayOfMonth()));
+        System.out.println("EoY: " + (today.lengthOfYear() - today.getDayOfYear()));
+        System.out.println("BDay: " + (LocalDate.of(2018, 11, 5).getDayOfYear() - today.getDayOfYear()));
+        System.out.println("EoL: " + (2060 - today.getYear()));
+
+
+    }
+}
